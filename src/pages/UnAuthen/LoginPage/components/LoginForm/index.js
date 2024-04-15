5aacad7dc067ee71ba2503a015d74264174a4c04
tunk <nguyenkhactu1996@gmail.com>
2024-04-15 08:50:33 +0700

    using formik

--- src/pages/UnAuthen/LoginPage/components/LoginForm/index.js
+++ src/pages/UnAuthen/LoginPage/components/LoginForm/index.js
@@ -5,59 +5,53 @@ import Card from "../../../../../common/components/Card";
 import {useState} from "react";
 import authService from "../../../../../common/api/authSerivce";
 import loadingGif from '../../../../../asset/img/loading.gif'
+import {useFormik} from "formik";
 
-function LoginForm(){
-  const [username, setUsername] = useState('')
-  const [password, setPassword] = useState('')
-  const [showLoading, setShowLoading] = useState(false)
+function LoginForm({onLogin}){
+  const [showLoading, setShowLoading] = useState(false);
 
-
-  const handleSubmitBtnClicked = (e) => {
-    e.preventDefault();
-    console.log('On form submit')
-    console.log('Username: ', username)
-    console.log('Password: ', password)
-    console.log('Starting login')
-    setShowLoading(true)
-    authService
-    .login(username, password)
-    .then(result => {
-      //When login return result
-      setShowLoading(false)
-      console.log('Login success with result: ', result)
-      console.log('Đăng nhập thành công!!!!')
-    })
-    console.log('Done login!!!!!!');
-  }
-
-  const onUsernameInputChanged = (e) => {
-    setUsername(e.target.value)
-  }
-
-  const onPasswordInputChanged = (e) => {
-    setPassword(e.target.value)
-  }
+  const loginForm = useFormik({
+    initialValues: {
+      username: '',
+      password: ''
+    },
+    onSubmit: (values) => {
+      const {username, password} = values;
+      setShowLoading(true)
+      authService
+      .login(username, password)
+      .then(result => {
+        //When login return result
+        setShowLoading(false)
+        console.log('Login success with result: ', result)
+        console.log('Đăng nhập thành công!!!!')
+      })
+    }
+  })
 
   return (
+
       <div className={"login-form-container"}>
         <div className={"login-form"}>
           <Card>
             <div className={"login-form-card"}>
               <img src={loginLogo} alt=""/>
-              <form>
+              <form onSubmit={loginForm.handleSubmit}>
                 <TextInput
-                    onChange={onUsernameInputChanged}
-                    value={username}
+                    onChange={loginForm.handleChange}
+                    name={'username'}
+                    value={loginForm.values.username}
                     type={"text"}
                     placeholder={"Tên đăng nhập"}
                 />
                 <TextInput
-                    onChange={onPasswordInputChanged}
-                    value={password}
+                    onChange={loginForm.handleChange}
+                    name ='password'
+                    value={loginForm.values.password}
                     type={"password"}
                     placeholder={"Mật khẩu"}
                 />
-                <button onClick={handleSubmitBtnClicked} type={"submit"}>Đăng nhập</button>
+                <button type={"submit"}>Đăng nhập</button>
                 <div>
                   {
                     showLoading && <img style={{marginTop: "20px"}} src={loadingGif} alt=""/>
